60a1234a1abd12167f4ac19dcab0ba88386eba54
Ruyher <163897714+Ruyher@users.noreply.github.com>
2024-08-26 17:10:34 -0300

    Update form-altas.php

--- from/form-altas.php
+++ from/form-altas.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Equipo - Torneo Nacional</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/formAltas.css">
 </head>
 <body>
     <div class="form-container">
