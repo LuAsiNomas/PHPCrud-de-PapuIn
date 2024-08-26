90f1429c533e04010caf72cf8451486a34a6793f
Ruyher <163897714+Ruyher@users.noreply.github.com>
2024-08-26 16:36:25 -0300

    Update form-altas.php

--- from/form-altas.php
+++ from/form-altas.php
@@ -3,31 +3,31 @@
 	<form class="altaInput" action="../server/altas.php" method="POST" enctype="multipart/form-data">
 		<div class="grupoInputs">
 			<img onclick="seleccionarArchivo()" src="" id="logo" alt="">
-			<input type="file" class="inputFile" name="foto"><br>
+			<input type="file" class="inputFile" name="foto" required="Ponga logo"><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Id de equipo" name="Equipo_ID"><br>
+			<input type="text" placeholder="Id de equipo" name="Equipo_ID" required="Texto vacío"><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Nombre de equipo" name="nombre_equipo"><br>
+			<input type="text" placeholder="Nombre de equipo" name="nombre_equipo" required="Texto vacío"><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Id líder" name="id_jug_lider"><br>
+			<input type="text" placeholder="Id líder" name="id_jug_lider" required="Texto vacío"><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Id jugador 2" name="idjug2"><br>
+			<input type="text" placeholder="Id jugador 2" name="idjug2" required="Texto vacío"><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Id jugador 3" name="idjug"><br>
+			<input type="text" placeholder="Id jugador 3" name="idjug" required="Texto vacío"><br>
 		</div>
 
 		<div class="grupoInputs">
-			<input type="text" placeholder="Provincia" name="provincia"><br>
+			<input type="text" placeholder="Provincia" name="provincia" required="Texto vacío"><br>
 		</div>
 
 		<div class="btn-submit">
