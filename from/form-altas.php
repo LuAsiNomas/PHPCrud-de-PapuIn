147af55888669a60c59d6a1d555c36588062213d
LuAsiNomas <lucas.salto.222@gmail.com>
2024-08-28 14:52:26 -0300

    arreglo para merge

---
+++ from/form-altas.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Equipo - Torneo Nacional</title>
+    <link rel="stylesheet" href="../css/formAltas.css">
+</head>
+<body>
+    <div class="form-container">
+        <h3 class="form-title">TORNEO NACIONAL</h3>
+        <form class="form-content" action="../server/altas.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <img onclick="seleccionarArchivo()" src="" id="logo" alt="Logo del equipo">
+                <input type="file" class="input-file" name="foto" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Id de equipo" name="Equipo_ID" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Nombre de equipo" name="nombre_equipo" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Id líder" name="id_jug_lider" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Id jugador 2" name="idjug2" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Id jugador 3" name="idjug" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Provincia" name="provincia" required>
+            </div>
+
+            <div class="form-group">
+                <input class="btn-submit" type="submit" value="REGISTRAR EQUIPO">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ from/form-modificacion.php
@@ -0,0 +1,46 @@
+<div class="form">
+	<h3 class="modificarTitulo">FORMULARIO DE MODIFICACION</h3>
+	<form class="modificarInput" action="../server/modificacion.php" method="POST" enctype="multipart/form-data">
+		<div class="container-select">
+			<div id="modalModificar" class=""></div>
+		</div>
+
+		Campos a modificar:<br>
+
+		<div class="grupoInputsCambiar">
+			<div class="imagenUsuario">
+				<img onclick="seleccionarArchivoModificar()" id="imgPerfilModificar" src="../images/fotoPerfil.png"
+					alt="">
+				<input type="file" name="foto" class="inputFileModificar">
+			</div>
+			<label class="labelInput">
+					<input type="text" class="cambioInput" id="nombre" placeholder="" name="nombre" required>
+					<spam class="spanInput">Nombre de equipo</spam>
+				</label>
+			<div class="grupoInputsModificar">
+				<label class="labelInput">
+					<input type="text" class="cambioInput" id="idlider" placeholder="" name="idlider" required>
+					<spam class="spanInput">ID Lider</spam>
+				</label>
+				<label class="labelInput">
+					<input type="text" class="cambioInput" id="id2" placeholder="" name="id2" required>
+					<spam class="spanInput">ID Jugador 2</spam>
+				</label>
+				<label class="labelInput">
+					<input type="text" class="cambioInput" id="id3" placeholder="" name="id3" required>
+					<spam class="spanInput">ID Jugador 3</spam>
+				</label>
+				<label class="labelInput">
+					<input type="text" class="cambioInput" id="provincia" placeholder="" name="provincia" required>
+					<spam class="spanInput">Provincia</spam>
+				</label>
+				
+				<input type="date" id="fecha" placeholder="fecha" name="fecha" required>
+
+				<div class="btn-submit">
+					<input type="submit" class="btn-modificar" value="Modificar">
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
