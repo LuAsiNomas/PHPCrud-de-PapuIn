e11a19656135843e1325279a571c6defb2ed3e53
LuAsiNomas <lucas.salto.222@gmail.com>
2024-09-04 15:08:15 -0300

    4/9-3

--- from/form-altas.php
+++ from/form-altas.php
@@ -11,8 +11,12 @@
         <h3 class="form-title">TORNEO NACIONAL</h3>
         <form class="form-content" action="../server/altas.php" method="POST" enctype="multipart/form-data">
             <div class="form-group">
-                <img onclick="seleccionarArchivo()" src="" id="logo" alt="Logo del equipo">
+                <img onclick="seleccionarArchivo()" src="../IMAGENES/mm.png" id="logo" alt="Logo del equipo">
                 <input type="file" class="input-file" name="foto" required>
+            </div>
+
+            <div class="form-group">
+                <input type="text" placeholder="Id de equipo" name="Equipo_ID" required>
             </div>
 
             <div class="form-group">
@@ -28,11 +32,37 @@
             </div>
 
             <div class="form-group">
-                <input type="text" placeholder="Id jugador 3" name="idjug" required>
+                <input type="text" placeholder="Id jugador 3" name="idjug3" required>
             </div>
 
             <div class="form-group">
-                <input type="text" placeholder="Provincia" name="provincia" required>
+                <select name="Provincia" id="prov" required>
+                    <option value="Disabled">SELECCIONAR PROVINCIA</option>
+                    <option value="1">Buenos Aires</option>
+                    <option value="2">Catamarca</option>
+                    <option value="3">Chaco</option>
+                    <option value="4">Chubut</option>
+                    <option value="5">Córdoba</option>
+                    <option value="6">Corrientes</option>
+                    <option value="7">Entre Ríos</option>
+                    <option value="8">Formosa</option>
+                    <option value="9">Jujuy</option>
+                    <option value="10">La Pampa</option>
+                    <option value="11">La Rioja</option>
+                    <option value="12">Mendoza</option>
+                    <option value="13">Misiones</option>
+                    <option value="14">Neuquén</option>
+                    <option value="15">Río Negro</option>
+                    <option value="16">Salta</option>
+                    <option value="17">San Juan</option>
+                    <option value="18">San Luis</option>
+                    <option value="19">Santa Cruz</option>
+                    <option value="20">Santa Fe</option>
+                    <option value="21">Santiago del Estero</option>
+                    <option value="22">Tierra del fuego</option>
+                    <option value="23">Tucumán</option>
+                    <option value="24">CABA</option>
+                </select>
             </div>
 
             <div class="form-group">
@@ -40,5 +70,7 @@
             </div>
         </form>
     </div>
+
+    <script src="../js/form-altas.js"></script>
 </body>
 </html>
