ec6d7924e6cc44711625d89e447465b081b6c79d
eri223223 <164930259+GuisoDeLentejas69@users.noreply.github.com>
2024-09-30 17:47:55 -0300

    Update conexion.php

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,12 +1,6 @@
 <?php
-<<<<<<< HEAD
 $base = "basarda";
 $Conexion = mysqli_connect("localhost", "root", "", $base);
 return $Conexion;
 
-=======
-$basarda = "gestionsubir";
-$Conexion = mysqli_connect("localhost", "root", "", $basarda);
-return $Conexion;
->>>>>>> main
 ?>
