d4b7115dfec68804c5bde96c55a193fc3caca7ce
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-10-14 15:05:05 -0300

    Update form-bajas.php

--- from/form-bajas.php
+++ from/form-bajas.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORMULARIO DE BAJA</title>
-    <link rel="stylesheet" href="/css/formBajas.css">
+    <link rel="stylesheet" href="../css/formBajas.css">
 </head>
 <body>
 
-<div class="formis">
+<div class="formis-container">
 	<h3 class="bajaTitulo">FORMULARIO DE BAJA</h3>
 	<form class="bajaInput" action="../server/bajas.php" method="POST">
 		<div class="grupoInputs">
