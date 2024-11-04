7f0e4c8e2b8451c702f4887bf427badbcfd0e697
= <164915174+Maximopo@users.noreply.github.com>
2024-11-04 17:43:10 -0300

    Update sv-fixture.php

--- server/sv-fixture.php
+++ server/sv-fixture.php
@@ -1,4 +1,4 @@
-<?php
+<//?php
 // Conexión a la base de datos
 
 include("../modelo/usuario.php");
@@ -25,3 +25,46 @@ if ($resultado->num_rows > 0) {
 // Cerrar conexión
 $conexion->close();
 ?>
+<//h3 class="modificarTitulo">FIXTURE<///h3>
+<//div class="left-side">
+        <//?php for ($i = 0; $i < 4; $i++): 
+        
+        include("../modelo/usuario.php");
+
+        $foto = $_FILES['foto'];
+        $noe = $_POST['nombre_equipo'];
+        $idl = $_POST['idlider'];
+        $id2 = $_POST['idjug2'];
+        $id3 = $_POST['idjug3'];
+        $fototamaño = $_FILES['fototamaño'];?>
+        
+        <//div class="team-card">
+                <//img src="<//?php echo $equipos[$foto]['logo']; ?>" alt="Logo del equipo" class="team-logo">
+        <//h3><//?php echo $equipos[$noe]['nombre_equipo']; ?><///h3>
+        <//p><//?php echo $equipos[$id1]['idlider']; ?><///p>
+        <//p><//?php echo $equipos[$id2]['idjug2']; ?><///p>
+        <//p><//?php echo $equipos[$id3]['idjug3']; ?><///p>
+            <///div>
+        <//?php endfor; ?>
+    <///div>
+        <//div class="right-side">
+        <//?php for ($i = 4; $i < 8; $i++): 
+
+            include("../modelo/usuario.php");
+        
+            $foto = $_FILES['foto'];
+            $noe = $_POST['nombre_equipo'];
+            $idl = $_POST['idlider'];
+            $id2 = $_POST['idjug2'];
+            $id3 = $_POST['idjug3'];
+            $fototamaño = $_FILES['fototamaño'];?>
+
+        <//div class="team-card">
+                <//img src="<//?php echo $equipos[$foto]['logo']; ?>" alt="Logo del equipo" class="team-logo">
+        <//h3><//?php echo $equipos[$noe]['nombre_equipo']; ?><///h3>
+        <//p><//?php echo $equipos[$id1]['idlider']; ?><///p>
+        <//p><//?php echo $equipos[$id2]['idjug2']; ?><///p>
+        <//p><//php echo $equipos[$id3]['idjug3']; ?><///p>
+            <///div>
+        <//?php endfor; ?>
+    <///div>
