0ded095c855dfda0e50e94685ee1732ee85491b8
= <164915174+Maximopo@users.noreply.github.com>
2024-11-04 16:17:38 -0300

    Delete cursor.js

--- js/cursor.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.getElementById('custom-cursor');
-    cursor.style.left = `${e.pageX}px`;
-    cursor.style.top = `${e.pageY}px`;
-});
