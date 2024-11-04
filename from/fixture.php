6585cad67ffb756b4a8c7003ecbc2ab676fd6ee9
= <164915174+Maximopo@users.noreply.github.com>
2024-11-04 17:43:13 -0300

    Update fixture.php

--- from/fixture.php
+++ from/fixture.php
@@ -3,54 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/fixture.css">
+    <link rel="stylesheet" href="/css/fixture.css">
 </head>
 <body>
-<div class="fixture-container">
-    <h3 class="modificarTitulo">FIXTURE</h3>
-    <div class="left-side">
-        <?php for ($i = 0; $i < 4; $i++): 
-        
-        include("../modelo/usuario.php");
-
-        $foto = $_FILES['foto'];
-        $noe = $_POST['nombre_equipo'];
-        $idl = $_POST['idlider'];
-        $id2 = $_POST['idjug2'];
-        $id3 = $_POST['idjug3'];
-        $fototamaño = $_FILES['fototamaño'];?>
-        
-            <div class="team-card">
-                <img src="<?php echo $equipos[$foto]['logo']; ?>" alt="Logo del equipo" class="team-logo">
-                <h3><?php echo $equipos[$noe]['nombre_equipo']; ?></h3>
-                <p><?php echo $equipos[$id1]['idlider']; ?></p>
-                <p><?php echo $equipos[$id2]['idjug2']; ?></p>
-                <p><?php echo $equipos[$id3]['idjug3']; ?></p>
-            </div>
-        <?php endfor; ?>
+<div class= "main">
+    <div class="fixture-container1">
+        <h3>Equipo 1</h3>
     </div>
-    <div class="right-side">
-        <?php for ($i = 4; $i < 8; $i++): 
-
-            include("../modelo/usuario.php");
-        
-            $foto = $_FILES['foto'];
-            $noe = $_POST['nombre_equipo'];
-            $idl = $_POST['idlider'];
-            $id2 = $_POST['idjug2'];
-            $id3 = $_POST['idjug3'];
-            $fototamaño = $_FILES['fototamaño'];?>
-
-            <div class="team-card">
-                <img src="<?php echo $equipos[$foto]['logo']; ?>" alt="Logo del equipo" class="team-logo">
-                <h3><?php echo $equipos[$noe]['nombre_equipo']; ?></h3>
-                <p><?php echo $equipos[$id1]['idlider']; ?></p>
-                <p><?php echo $equipos[$id2]['idjug2']; ?></p>
-                <p><?php echo $equipos[$id3]['idjug3']; ?></p>
-            </div>
-        <?php endfor; ?>
+    <div class="fixture-container1">
+        <h3>Equipo 1</h3>
     </div>
-</div>
+    <div class="fixture-container1">
+        <h3>Equipo 1</h3>
+    </div>
+    <div class="fixture-container1">
+        <h3>Equipo 1</h3>
+    </div>
+    <div class="fixture-container2">
+    </div>
+    <div class="fixture-container2">
+    </div>
+    <div class="fixture-container2">
+    </div>
+    <div class="fixture-container2">
+    </div>
 </div>	
 </body>
 </html>
