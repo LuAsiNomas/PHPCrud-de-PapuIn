14043c180c52d0fc3e4bc5bd56afa4cb3084f744
= <>
2024-11-04 15:25:45 -0300

    Update respuesta-alta.php

--- from/respuestas-server/respuesta-alta.php
+++ from/respuestas-server/respuesta-alta.php
@@ -1,7 +1,7 @@
 <div class="Rcontainer">
     <div class="Rbox">
         <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
-        <h3 class="Rcuerpo">El registros se guardaron correctamente</h3>
+        <h3 class="Rcuerpo">Los registros se guardaron correctamente</h3>
         <a href="../index.php" class="cerrar">Cerrar</a>
     </div>
 </div>
