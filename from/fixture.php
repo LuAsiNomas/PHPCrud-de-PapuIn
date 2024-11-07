fe06d5d6695a63c51f17e8fa86c4081806607be4
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-11-07 12:02:45 -0300

    Update fixture.php

--- from/fixture.php
+++ from/fixture.php
@@ -9,57 +9,74 @@
     <div class="formis">
         <div class="fixturegroup1">
             <h3 class="fase-text">Cuartos</h3>
+            <div class="fixture-container1"> 
+                <h3>Equipo 1</h3>
+            </div>
+            <div class="linea-horizontal1"></div>
             <div class="fixture-container1">
                 <h3>Equipo 1</h3>
             </div>
+            <div class="linea-horizontal2"></div>
             <div class="fixture-container1">
                 <h3>Equipo 1</h3>
             </div>
+            <div class="linea-horizontal3"></div>
             <div class="fixture-container1">
                 <h3>Equipo 1</h3>
             </div>
-            <div class="fixture-container1">
-                <h3>Equipo 1</h3>
-            </div>
+            <div class="linea-horizontal4"></div>
         </div>
         <div class="fixturegroup2">
         <h3 class="fase-text">Cuartos</h3>
             <div class="fixture-container1">
                 <h3>Equipo 2</h3>
             </div>
+            <div class="linea-horizontal5"></div>
             <div class="fixture-container1">
                 <h3>Equipo 2</h3>
             </div>
+            <div class="linea-horizontal6"></div>
             <div class="fixture-container1">
                 <h3>Equipo 2</h3>
             </div>
+            <div class="linea-horizontal7"></div>
             <div class="fixture-container1">
                 <h3>Equipo 2</h3>
             </div>
+            <div class="linea-horizontal8"></div>
         </div>
         <div class="fixturegroup3">
-            <h3 class="fase-text">Semifinales</h3>
-            <div class="fixture-container1">
+            <h3 class="fase-text1">Semifinales</h3>
+            <div class="fixture-container2">
                 <h3>Equipo 1</h3>
             </div>
-            <div class="fixture-container1">
+            <div class="linea-horizontal9"></div>
+            <div class="linea-vertical1"></div>
+            <div class="fixture-container2">
                 <h3>Equipo 1</h3>
             </div>
+            <div class="linea-horizontal10"></div>
+            <div class="linea-vertical2"></div>
         </div>
         <div class="fixturegroup4">
             <h3 class="fase-text">Semifinales</h3>
-            <div class="fixture-container1">
+            <div class="fixture-container2">
                 <h3>Equipo 1</h3>
             </div>
-            <div class="fixture-container1">
+            <div class="linea-horizontal11"></div>
+            <div class="linea-vertical3"></div>
+            <div class="fixture-container2">
                 <h3>Equipo 1</h3>
             </div>
+            <div class="linea-horizontal12"></div>
+            <div class="linea-vertical4"></div>
         </div>
         <div class="fixturegroup5">
             <h3 class="fase-text">Final</h3>
             <div class="fixture-container1">
                 <h3>Equipo 1</h3>
             </div>
+            <div class="linea-horizontal13"></div>
         </div>
     </div>
 </body>
