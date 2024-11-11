d943bec6189f0bf5f1b8431f6ad2c3ad99df77b1
lean <164915174+Maximopo@users.noreply.github.com>
2024-11-11 15:23:56 -0300

    Update form-modificacion.php

--- from/form-modificacion.php
+++ from/form-modificacion.php
@@ -1,56 +1,50 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FORMULARIO DE MODIFICACION</title>
-    <link rel="stylesheet" href="../css/formModificacion.css">
-</head>
-<body>
 <div class="form">
 	<h3 class="modificarTitulo">FORMULARIO DE MODIFICACION</h3>
-	<form class="modificarInput" id="modificacionForm" action="server/modificacion.php" method="POST" enctype="multipart/form-data">
+	<form class="modificarInput" action="../server/modificacion.php" method="POST" enctype="multipart/form-data">
 		<div class="container-select">
-			<?php
-			include("modelo/usuario.php");
-			echo getUsuarioUsersNamesModificar();	
-			?>
 			<div id="modalModificar" class=""></div>
 		</div>
+
+		<?php
+		include("modelo/usuario.php");
+			echo getUsuarioUsersNamesModificar();
+		?>
+			<div id="modalModificar" class=""></div>
 
 		Campos a modificar:<br>
 
 		<div class="grupoInputsCambiar">
 			<div class="imagenUsuario">
-				<img onclick="seleccionarArchivoModificar()" id="imgPerfilModificar" src="../images/fotoPerfil.png"
+				<img onclick="seleccionarArchivoModificar()" id="iimgPerfilModificar" src="../images/fotoPerfil.png"
 					alt="">
 				<input type="file" name="foto" class="inputFileModificar">
 			</div>
 			<label class="labelInput">
-					<input type="text" class="cambioInput" id="nombre" placeholder="Nombre de equipo" name="nombre" required>
+					<input type="text" class="cambioInput" id="nombre" placeholder="" name="nombre" required>
+					<spam class="spanInput">Nombre de equipo</spam>
 				</label>
 			<div class="grupoInputsModificar">
 				<label class="labelInput">
-					<input type="text" class="cambioInput" id="idlider" placeholder="ID Lider" name="idlider" required>
+					<input type="text" class="cambioInput" id="idlider" placeholder="" name="idlider" required>
+					<spam class="spanInput">ID Lider</spam>
 				</label>
 				<label class="labelInput">
-					<input type="text" class="cambioInput" id="id2" placeholder="ID Jugador 2" name="id2" required>
+					<input type="text" class="cambioInput" id="id2" placeholder="" name="id2" required>
+					<spam class="spanInput">ID Jugador 2</spam>
 				</label>
 				<label class="labelInput">
-					<input type="text" class="cambioInput" id="id3" placeholder="ID Jugador 3" name="id3" required>
+					<input type="text" class="cambioInput" id="id3" placeholder="" name="id3" required>
+					<spam class="spanInput">ID Jugador 3</spam>
 				</label>
 				<label class="labelInput">
-					<input type="text" class="cambioInput" id="provincia" placeholder="Provincia" name="provincia" required>
-					<spam class="spanInput"></spam>
+					<input type="text" class="cambioInput" id="provincia" placeholder="" name="provincia" required>
+					<spam class="spanInput">Provincia</spam>
 				</label>
 				
-				<div class="btn-submit-modificar">
+				<div class="btn-submit">
 					<input type="submit" class="btn-modificar" value="Modificar">
 				</div>
 			</div>
 		</div>
 	</form>
-    <script src="../js/sonido.js"></script>
-</div>	
-</body>
-</html>
+</div>
