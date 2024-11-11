b14de07f211866ea061655ab62b13c31bd9078d4
lean <164915174+Maximopo@users.noreply.github.com>
2024-11-11 17:06:53 -0300

    Update altas.php

--- server/altas.php
+++ server/altas.php
@@ -35,18 +35,3 @@ if (strlen($result) > 5) {
     return include("../from/respuestas-server/respuesta-alta.php");
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-
-<body>
-	<a href="form-altas.php">Volver</a>
-
-</body>
-
-</html>
