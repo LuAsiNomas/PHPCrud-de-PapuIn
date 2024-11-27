3f5b7e8694ff9dfbcb5bb270c69e9e0cd56a3ed3
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-11-27 17:54:34 -0300

    Create fixture.php

---
+++ from/fixture.php
@@ -0,0 +1,115 @@
+OCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/fixture.css">
+</head>
+<body>
+    
+    <div class="formis">
+        <div class="fixturegroup1">
+            <h3 class="fase-text">Cuartos</h3>
+            <div class="fixture-container1"> 
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal1"></div>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal2"></div>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal3"></div>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal4"></div>
+        </div>
+        <div class="fixturegroup2">
+        <h3 class="fase-text">Cuartos</h3>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal5"></div>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal6"></div>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                    </select>
+            </div>
+            <div class="linea-horizontal7"></div>
+            <div class="fixture-container1">
+            <select name="equipos" class="btn-equipos" required>
+                    <option value="Disabled">EQUIPOS</option>
+                </select>
+            </div>
+            <div class="linea-horizontal8"></div>
+        </div>
+        <div class="fixturegroup3">
+            <h3 class="fase-text1">Semifinales</h3>
+            <div class="fixture-containera2">
+                <h3>Equipo 2</h3>
+            </div>
+            <div class="linea-horizontala9"></div>
+            <div class="linea-horizontalb9"></div>
+            <div class="linea-vertical1"></div>
+            <div class="fixture-containerb2">
+                <h3>Equipo 1</h3>
+            </div>
+            <div class="linea-horizontala10"></div>
+            <div class="linea-horizontalb10"></div>
+            <div class="linea-vertical2"></div>
+        </div>
+        <div class="fixturegroup4">
+            <h3 class="fase-text2">Semifinales</h3>
+            <div class="fixture-containera3">
+                <h3>Equipo 4</h3>
+            </div>
+            <div class="linea-horizontala11"></div>
+            <div class="linea-horizontalb11"></div>
+            <div class="linea-vertical3"></div>
+            <div class="fixture-containerb3">
+                <h3>Equipo 6</h3>
+            </div>
+            <div class="linea-horizontala12"></div>
+            <div class="linea-horizontalb12"></div>
+            <div class="linea-vertical4"></div>
+        </div>
+        <div class="fixturegroup5">
+            <h3 class="fase-text-final">Final</h3>
+            <div class="fixture-containera4">
+                <h3>Equipo 1</h3>
+            </div>
+            <div class="fixture-containerb4">
+                <h3>Equipo 1</h3>
+            </div>
+            <div class="linea-horizontal13"></div>
+            <div class="linea-vertical5"></div>
+            <div class="linea-vertical6"></div>
+            <div class="linea-vertical7"></div>
+            <div class="linea-vertical8"></div>
+            <div class="linea-vertical9"></div>
+            <div class="fixture-containerCampeon">
+                <h3>Campeon</h3>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
